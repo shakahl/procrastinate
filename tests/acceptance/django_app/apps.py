8af63263312397d42464dc407f27293259d2c0da
Joachim Jablon <ewjoachim@gmail.com>
2023-03-04 17:43:32 +0100

    Move test Django app, add model

---
+++ tests/acceptance/django_app/apps.py
@@ -0,0 +1,5 @@
+from django import apps
+
+
+class TestApp(apps.AppConfig):
+    name = "tests.acceptance.django_app"
--- tests/acceptance/django_app/settings.py
+++ tests/acceptance/django_app/settings.py
@@ -5,9 +5,15 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.environ.get("PGDATABASE", "procrastinate"),
+        "TEST": {
+            "NAME": os.environ.get("PGDATABASE", "procrastinate") + "_django_test"
+        },
     },
 }
 
 INSTALLED_APPS = [
     "procrastinate.contrib.django",
+    "tests.acceptance.django_app",
 ]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
