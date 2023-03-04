fdcdf420285e26075c8b880af4b19cc3363d9491
Joachim Jablon <ewjoachim@gmail.com>
2023-03-04 17:42:02 +0100

    Fix Django deprecation warning

--- procrastinate/contrib/django/__init__.py
+++ procrastinate/contrib/django/__init__.py
@@ -1,4 +1,8 @@
+import django
+
 from .utils import connector_params
 
 __all__ = ["connector_params"]
-default_app_config = "procrastinate.contrib.django.apps.ProcrastinateConfig"
+
+if django.VERSION < (3, 2):
+    default_app_config = "procrastinate.contrib.django.apps.ProcrastinateConfig"
